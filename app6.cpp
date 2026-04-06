b3c4f7b17792a36d5d82a414f4543bd224561829
asyiffaurrohman <akungithub934@gmail.com>
2026-04-06 12:50:58 +0700

    15

---
+++ app6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string Desa;
+    string Kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    //Deklarasi variabel struct
+    mahasiswa mhs;
+    
+}

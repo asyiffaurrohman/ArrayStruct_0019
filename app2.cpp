28f0d01cc15f09bf8ae9b5748be223f3acb4cf5c
asyiffaurrohman <akungithub934@gmail.com>
2026-04-06 12:43:00 +0700

    5

--- app2.cpp
+++ app2.cpp
@@ -12,4 +12,9 @@ int main() {
     // cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
 
-   
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
+
+    
+}

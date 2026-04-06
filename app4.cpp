66284c04fd46ece1cd4179f254623200166a979b
asyiffaurrohman <akungithub934@gmail.com>
2026-04-06 12:48:16 +0700

    12

--- app4.cpp
+++ app4.cpp
@@ -12,4 +12,8 @@
         cin >> datamu[i];
     }
 
-    
+    // menampilkan data dengan perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " : " << datamu[i] << endl;
+    }

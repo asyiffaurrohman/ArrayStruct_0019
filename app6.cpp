1484c5305f6f28756c62889421428e692997096a
asyiffaurrohman <akungithub934@gmail.com>
2026-04-06 12:51:27 +0700

    16

--- app6.cpp
+++ app6.cpp
@@ -19,5 +19,19 @@ int main()
 {
     //Deklarasi variabel struct
     mahasiswa mhs;
-    
+    //Mengisi data
+    cout << "Isikan Data Nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan Data Kota : ";
+    getline(cin, mhs.alamat.Kota);
+    cout << "Isikan Data Desa : ";
+    getline(cin, mhs.alamat.Desa);
+    cout << "Isikan Data Usia : ";
+    cin >> mhs.umur;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Kota : " << mhs.alamat.Kota << endl;
+    cout << "Desa : " << mhs.alamat.Desa << endl;
+    cout << "Usia : " << mhs.umur << endl;
 }
